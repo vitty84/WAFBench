03966f0be961ea2f2ded01e32dbb1ab73aef8172
Ze Gan <zegan@microsoft.com>
2018-08-01 00:07:56 +0800

    add a sample about that WB send packets followd by yaml file

---
+++ sample/WB-SEND-PACKET-FROM-YAML.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+basepath=$(cd `dirname $0`; pwd)
+echo "Send custom traffics with yaml format"
+echo "command: ../Generator/YAML_generator.py packets/test-1-packet.yaml -o /tmp/packet.pkt"
+echo "command: wb -F /tmp/packet.pkt -c 20 -t 5 10.0.1.44:12701"
+../Generator/YAML_generator.py packets//test-1-packet.yaml -o /tmp/packet.pkt
+wb -F /tmp/packet.pkt -c 20 -t 5 10.0.1.44:12701
