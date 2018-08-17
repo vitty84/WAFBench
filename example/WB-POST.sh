864d3b4ec2da73279fbb16513771d0fca2e2ee7c
zegan <zegan@microsoft.com>
2018-08-17 14:59:37 +0800

    fix example/WB-POST content-type

--- example/WB-POST.sh
+++ example/WB-POST.sh
@@ -5,5 +5,5 @@
 
 basepath=$(cd `dirname $0`; pwd)
 echo "Doing AB-like POST testing"
-echo "command: wb -t 10 -c 10 -p packets/requestbody2kb.json 10.0.1.44:12701"
-wb -t 10 -c 10 -p packets/requestbody2kb.json 10.0.1.44:12701
+echo "command: wb -t 10 -c 10 -p packets/requestbody2kb.json -T application/json 10.0.1.44:12701"
+wb -t 10 -c 10 -p packets/requestbody2kb.json -T application/json 10.0.1.44:12701
